5cdc0a0765c5b16364e986982b2a5352036d2e55
rjorton <richardorton@hotmail.com>
2020-01-31 21:33:04 +0000

    Backup before major changes

--- seqfeats_viz.py
+++ seqfeats_viz.py
@@ -10,37 +10,12 @@ import pandas as pd
 import time
 import re
 
-# Start the dash app
 app = dash.Dash()
 # For testing vv
 pd.set_option('display.expand_frame_repr', False)
-# CSS is taken from this video https://www.youtube.com/watch?v=f2qUWgq7fb8&feature=youtu.be
-# and app layout is somewhat similar.
 app.css.config.serve_locally = False
 app.css.append_css({'external_url': 'https://codepen.io/amyoshino/pen/jzXypZ.css'})
-# app.scripts.config.serve_locally = True
-
-# The layout of the dash app
-#   Title                          filSep  Header       Drag and Drop
-#                                  |Dropdown| |dropdown|
-##################################################################
-#   Xlabel      Ylabel      CategoryLabel       TextLabel
-#  | Dropdown | | Dropdown |  | Dropdown |   |Dropdown|
-###################################################################
-#                               GRAPH
-#                               GRAPH
-#                               GRAPH
-#                               GRAPH
-#                               GRAPH
-#####################################################################
-#   Logtransform Label    Search Bar
-#   |   Dropdown |       | Seach here               | |Button |
-#####################################################################
-#                             DATATable
-#                             DATATable
-#                             DATATable
-#                             DATATable
-#####################################################################
+# CSS is taken from this video https://www.youtube.com/watch?v=f2qUWgq7fb8&feature=youtu.be and app layout is somewhat similar.
 
 app.layout = html.Div([
     html.Div(id="hidden_data", style={'display': 'none'}),
@@ -48,20 +23,18 @@ app.layout = html.Div([
     html.Div([
         html.Div([
            html.H3(
-               children='ScatterVis.\n',
-
+               children='SeqFeatsViz'
            ),
            html.P(
-               children='An interactive visualisation of biological data using python / dash.'
-
+               children='Interactive visualisation of virus dinucleotide biases using python / dash.'
            )
         ], className='seven columns'),
         html.Div([
             html.H6(
-                children='Known issues.'
+                children='Known issues:'
             ),
             html.P(
-                children='- Toggling the traces in the legend may cause points to disappear with datasets larger than 10k points.'
+                children='- Toggling series in the legend may cause points to disappear with datasets > 10k points.'
 
             ),
             html.P(
@@ -80,7 +53,7 @@ app.layout = html.Div([
                 ],
                 value='tab_sep'
             )
-        ], className='one columns'),
+        ], className='two columns'),
         html.Div([
             html.Label('Header Row'),
             dcc.Dropdown(
@@ -91,7 +64,7 @@ app.layout = html.Div([
                 ],
                 value='header_1'
             )
-        ], className='one columns'),
+        ], className='two columns'),
         html.Div([
            dcc.Upload(
             id="csv-upload",
@@ -114,6 +87,7 @@ app.layout = html.Div([
            )
         ], id='hidden_upload', style={'visibility': 'hidden'})
     ], className='row'),
+    #
     html.Div([
         html.Div([
             html.Label('X-Values'),
@@ -154,7 +128,7 @@ app.layout = html.Div([
                 id='genbank_drop',
                 value="N/A"
             )
-        ], id='ncbi_div', className='two columns'),
+        ], id='genbank_div', className='two columns'),
         html.Div([
             html.Label('Ensembl-ID'),
             dcc.Dropdown(
@@ -178,22 +152,21 @@ app.layout = html.Div([
             html.Label('Search-In'),
             dcc.Dropdown(
                 id='searchin_drop',
-                multi=True,
+                multi=True
             )
         ], className='four columns'),
-    ], id='ncbi_hidden_div', style={'visibility': 'hidden'}, className='row'),
+    ], id='main_div2', style={'visibility': 'hidden'}, className='row'),
     #
     html.Div([
         html.Div([
             dcc.Graph(
-                id="Scatter-Graph",
-                # clickData={'points'}
+                id="Scatter-Graph"
             )
         ], className='twelve columns')
     ], style={'border-style': 'solid', 'border-width': '2px', 'margin-top': '5px'}, className='row'),
     html.Div([
         html.Div([
-            html.Label('Serach for points in graph:',
+            html.Label('Search graph:',
                        style={'text-align': 'center',
                               'margin-top': '5px'})
         ], className='two columns'),
@@ -208,29 +181,34 @@ app.layout = html.Div([
                     'margin-top': '2px'
                 }
             )
-        ], className='five columns'),
+        ], className='three columns'),
         html.Div([
-            html.Button('Search.',
-                        id='search_button',
-                        style={'margin-top': '2px', 'background-color': '#FEFEFE'}
-                        ),
             html.Button(
-                'Clear.',
+                'Search',
+                id='search_button',
+                style={'margin-top': '2px', 'background-color': '#FEFEFE'}
+            )
+        ], className='two columns'),
+        html.Div([
+            html.Button(
+                'Clear',
                 id='clear_search',
                 style={
                     'margin-top': '2px',
                     'background-color': '#FEFEFE'
-
                 }
-            ),
-            html.Button('Clear Table.',
-                        id='clear_button',
-                        style={
-                            'margin-top': '2px',
-                            'background-color': '#FEFEFE'
-                        }
-                        ),
-        ], className='four columns')
+            )
+        ], className='two columns'),
+        html.Div([
+            html.Button(
+                'Clear Table',
+                id='clear_button',
+                style={
+                    'margin-top': '2px',
+                    'background-color': '#FEFEFE'
+                }
+            )
+        ], className='two columns')
     ], style={'background-color': '#E6E6E6', 'border-style': 'solid', 'border-top-style': 'none', 'border-width': '2px', 'padding-bottom': '10px'}, className='row'),
     html.Div([
         html.Div([], style={'margin-top': '10px', 'background-color': '#E6E6E6', 'border-style': 'solid', 'border-width': '2px', 'visibility': 'none'}, className='twelve columns', id='dtable_div')
@@ -238,7 +216,7 @@ app.layout = html.Div([
 ])
 
 
-# Function to pull a dataframe from a user uploaded csv
+# Function to pull a dataframe from a user uploaded csv or tsv
 def parse_csv(contents, filename, sep_radio, header):
     # Don't do anything if the variables aren't set
     if contents is None:
@@ -246,27 +224,23 @@ def parse_csv(contents, filename, sep_radio, header):
     if sep_radio is None:
         exit(1)
 
-    # If the separator drop drown is CSV / TSV do the corresponding split.
-    content_type, content_string = contents.split(",")
-
     cont_sep = ","
     if sep_radio == "comma_sep":
         cont_sep = ","
     elif sep_radio == "tab_sep":
         cont_sep = "\t"
 
+    # this does not split the data, but splits the file info - so always ,
+    content_type, content_string = contents.split(",")
     # Decode the file
     decoded = base64.b64decode(content_string)
 
     # Make a data frame with pandas
-    # Check if the user has specified a header or not.
     if header == "header_1":
         df = pd.read_csv(io.StringIO(decoded.decode('utf8')), header=0, sep=cont_sep)
     else:
         df = pd.read_csv(io.StringIO(decoded.decode('utf8')), header=None, sep=cont_sep)
-
-        # If there is no header make a new header like this
-        # Column 1 Column 2 etc, etc
+        # If there is no header make a new header like this: Column 1 Column 2 etc, etc
         df.columns = ["Column " + str(i) for i in range(1, len(df.columns.values) + 1)]
 
     return df
@@ -276,53 +250,38 @@ def parse_csv(contents, filename, sep_radio, header):
 # https://github.com/plotly/dash-table-experiments/pull/11
 
 
+# Populate values in SearchIn into Ensembl if SearchIn changes
 @app.callback(
     Output('ensembl_drop', 'options'),
     [Input('searchin_drop', 'value')]
 )
 def update_ensemble(drop):
     if drop is None or len(drop) == 0:
+        # return []
         exit(1)
     else:
         return [{'label': i, 'value': i} for i in drop]
 
-
-@app.callback(
-    Output('ensembl_div', 'style'),
-    [Input('searchin_drop', 'value')]
-)
-def show_ensembl(drop):
-    if drop is None or len(drop) == 0:
-        exit(1)
-    else:
-        return {'visiblity': 'show', 'margin-top': '10px'}
+# Removed the show_ensembl call back as not needed?
 
 
-#
-
+# Populate values in SearchIn into Genbank if SearchIn changes
 @app.callback(
     Output('genbank_drop', 'options'),
     [Input('searchin_drop', 'value')]
 )
-def update_ncbi(drop):
+def update_genbank(drop):
     if drop is None or len(drop) == 0:
+        # return []
         exit(1)
     else:
         return [{'label': i, 'value': i} for i in drop]
 
 
-@app.callback(
-    Output('ncbi_div', 'style'),
-    [Input('searchin_drop', 'value')]
-)
-def show_ncbi(drop):
-    if drop is None or len(drop) == 0:
-        exit(1)
-    else:
-        return {'visiblity': 'visible', 'margin-top': '10px'}
+# Removed the show_ncbi callback as not needed?
 
 
-# Call back to show upload after values set
+# Show file upload button if header & separator are set
 @app.callback(
     Output('hidden_upload', 'style'),
     [Input('sep_drop', 'value'),
@@ -335,20 +294,19 @@ def show_upload(v1, v2):
         return {'visibility': 'visible'}
 
 
-# Call back to upload df to hidden div
+# Parse the data file when upload button/select file hit
 @app.callback(
     Output('hidden_data', 'children'),
     [Input("csv-upload", "contents"), Input("csv-upload", "filename")],
     [State("sep_drop", 'value'), State("header_drop", 'value')]
 )
-def get_file(file_contents, file_name, radio, header):
-    df = parse_csv(file_contents, file_name, radio, header)
-
+def get_file(file_contents, file_name, separator, header):
+    df = parse_csv(file_contents, file_name, separator, header)
     # Read the csv and save it in the hidden div as json.
-    # Needs to be json.
     return df.to_json(date_format='iso', orient='split')
 
 
+# RJO - Not sure what exactly - must be using header names for dropdown values
 @app.callback(
     Output('hidden_header', 'children'),
     [Input('hidden_data', 'children')]
@@ -361,15 +319,16 @@ def store_headers(data):
         return json.dumps(list(df.columns.values))
 
 
-# Call back to show div after successful file upload
+# Callback to show main_div (x-axis, y-axis etc) after successful file upload
 @app.callback(
-    Output('ncbi_hidden_div', 'style'),
+    Output('main_div2', 'style'),
     [Input('hidden_data', 'children')]
 )
 def show_under_main(data):
     return {'background-color': '#E6E6E6', 'border-style': 'solid', 'border-top-style': 'none', 'border-width': '2px', 'visibility': 'visible'}
 
 
+# Callback to show main_div2 (ensembl, genbank etc) after successful file upload
 @app.callback(
     Output('main_div', 'style'),
     [Input('hidden_data', 'children')]
@@ -378,8 +337,7 @@ def show_main_div(data):
     return {'background-color': '#E6E6E6', 'border-style': 'solid', 'border-bottom-style': 'none', 'border-width': '2px', 'visibility': 'visible'}
 
 
-# Callbacks to populate drop downs
-# CATEGORY
+# Series - update??
 @app.callback(
     Output('searchin_drop', 'options'),
     [Input('hidden_header', 'children')]
@@ -391,23 +349,23 @@ def update_multi(data):
     return [{'label': i, 'value': i} for i in name]
 
 
+# Searchin - initial?
 @app.callback(
     Output('searchin_drop', 'value'),
     [Input('hidden_header', 'children')]
 )
-def update_x_value(data):
+def update_searchin(data):
     names = json.loads(data)
     return [i for i in names[3:] if i.lower().startswith("desc") or i.lower().startswith("ensembl") or i.lower().startswith("genbank") or i.lower().startswith("accession")]
 
 
 # XAXIS
+# This call back and all the other dropdown updates follow the same workflow as this.
 @app.callback(
     Output('xaxis_drop', 'options'),
     [Input('hidden_header', 'children')]
 
 )
-# This call back and all the other dropdown updates follow the same workflow as this.
-# Read the comments for this one vvvvvvvv
 def update_x_drop(data):
     # Decode the json dataframe which was stored on users browser.
     names = json.loads(data)
@@ -415,6 +373,7 @@ def update_x_drop(data):
     return [{'label': i, 'value': i} for i in names]
 
 
+# XAXIS Initial call sets to [0]
 @app.callback(
     Output('xaxis_drop', 'value'),
     [Input('hidden_header', 'children')]
@@ -422,27 +381,6 @@ def update_x_drop(data):
 def update_x_value(data):
     names = json.loads(data)
     return names[0]
-
-
-@app.callback(
-    Output('filter_drop', 'options'),
-    [Input('hidden_header', 'children')]
-)
-def update_spec_drop(data):
-    names = json.loads(data)
-    return [{'label': i, 'value': i} for i in names]
-
-
-@app.callback(
-    Output('filter_value', 'options'),
-    [Input('filter_drop', 'value'),
-     Input('hidden_data', 'children')]
-)
-def update_spec_graph(val, data):
-    df = pd.read_json(data, orient='split')
-    if val is None:
-        exit(1)
-    return [{'label': i, 'value': i} for i in df[val].unique()]
 
 
 # YAXIS
@@ -455,6 +393,7 @@ def update_y_drop(data):
     return [{'label': i, 'value': i} for i in names]
 
 
+# YAXIS Initial call sets to [1]
 @app.callback(
     Output('yaxis_drop', 'value'),
     [Input('hidden_header', 'children')]
@@ -464,7 +403,30 @@ def update_x_value(data):
     return names[1]
 
 
-# CATEGORY
+# Filter column/drop - no intial value set
+@app.callback(
+    Output('filter_drop', 'options'),
+    [Input('hidden_header', 'children')]
+)
+def update_spec_drop(data):
+    names = json.loads(data)
+    return [{'label': i, 'value': i} for i in names]
+
+
+# Filter value (from within the filter column) - no intial value set
+@app.callback(
+    Output('filter_value', 'options'),
+    [Input('filter_drop', 'value'),
+     Input('hidden_data', 'children')]
+)
+def update_spec_graph(val, data):
+    df = pd.read_json(data, orient='split')
+    if val is None:
+        exit(1)
+    return [{'label': i, 'value': i} for i in df[val].unique()]
+
+
+# Series
 @app.callback(
     Output('series_drop', 'options'),
     [Input('hidden_header', 'children')]
@@ -475,6 +437,7 @@ def update_cat_drop(data):
     return [{'label': i, 'value': i} for i in names]
 
 
+# Series Initial call sets to [2]
 @app.callback(
     Output('series_drop', 'value'),
     [Input('hidden_header', 'children')]
@@ -484,6 +447,7 @@ def update_x_value(data):
     return names[2]
 
 
+# Log axes, x, y, x&y, reset
 @app.callback(
     Output('log_drop', 'options'),
     [Input('xaxis_drop', 'value'),
@@ -500,6 +464,7 @@ def update_logs(x, y):
     return [{'label': i, 'value': i} for i in names]
 
 
+# Sort axes, x or y, or rest
 @app.callback(
     Output('sort_drop', 'options'),
     [Input('xaxis_drop', 'value'),
@@ -528,11 +493,9 @@ def update_numeric(x, y):
      Input('clear_search', 'n_clicks_timestamp')],
     [State('search_box', 'value'),
      State('filter_drop', 'value')]
-
 )
-# Gets input values from all of the drop down menus.
-# Updates when any of these values are changed.
-def update_graph(xaxis, yaxis, category, hidden, mval, search_button, logs, nums, species, clear_time, search, species_val):
+# Gets input values from all of the drop down menus - Updates when any of these values are changed.
+def update_graph(xaxis, yaxis, series, hidden, mval, search_button, logs, nums, filter_val, clear_time, search, filter_col):
     # If any of the three values are not set don't do anything.
     if xaxis is None or yaxis is None or mval is None:
         exit(1)
@@ -544,10 +507,10 @@ def update_graph(xaxis, yaxis, category, hidden, mval, search_button, logs, nums
 
     # Read the json dataframe stored in the users browser.
     df = pd.read_json(hidden, orient='split')
-    if species is None:
+    if filter_val is None:
         df = df
     else:
-        df = df[df[species_val] == species]
+        df = df[df[filter_col] == filter_val]
 
     df_list = []
 
@@ -564,11 +527,10 @@ def update_graph(xaxis, yaxis, category, hidden, mval, search_button, logs, nums
             df_list.append(df[df[i].str.contains(search, flags=re.IGNORECASE, na=False)])
 
         # You get separate dataframes for each separate text column so concat them together.
+        # RJO - What if same entry appears in each frame?? multiple rows of same entry
         df = pd.concat(df_list)
-    print(search)
-    # Functions to deal with log scaling the plots. Both follow the same logic
-    # So just read the comments for this one.
 
+    # Functions to deal with log scaling the plots. Both follow the same logic
     def logs_x(lst, x, y, n):
         if n == x or n is None or n == "Reset Axes":
             not_num = True
@@ -631,8 +593,8 @@ def update_graph(xaxis, yaxis, category, hidden, mval, search_button, logs, nums
             df = pd.DataFrame(data)
             return df.iloc[:, 0]
 
-    # If the user hasn't specified a category colum in the drop down do this
-    if category == "N/A" or category is None:
+    # If the user hasn't specified a series colum in the drop down do this
+    if series == "N/A" or series is None:
         traces = []
         # If user has specified text columns do this.
         if "N/A" not in mval:
@@ -667,10 +629,10 @@ def update_graph(xaxis, yaxis, category, hidden, mval, search_button, logs, nums
     # If the user has specified some categories do this.
     else:
         traces = []
-        # For each unique item in the category column the user has specfied
+        # For each unique item in the series column the user has specfied
         # subset the dataframe to rows with this value.
-        for i in df[category].unique():
-            subset_df = df[df[category] == i]
+        for i in df[series].unique():
+            subset_df = df[df[series] == i]
 
             # If there's is text specified do the same as we did above.
             if "N/A" not in mval:
@@ -709,7 +671,6 @@ def update_graph(xaxis, yaxis, category, hidden, mval, search_button, logs, nums
 
 
 # Callback to update table
-
 @app.callback(
     Output('dtable_div', 'children'),
     [Input('Scatter-Graph', 'clickData'), Input('Scatter-Graph', 'selectedData')],
@@ -719,8 +680,6 @@ def update_graph(xaxis, yaxis, category, hidden, mval, search_button, logs, nums
      State('ensembl_drop', 'value'),
      State('genbank_drop', 'value')]
 )
-
-
 # Update the table with data from either / both of a box / lassoo select or a click.
 def update_table(clicked, selected,  mval, xval, yval, ensembl, genbank):
 
@@ -851,6 +810,7 @@ def clear_table(click, select, button):
                 'visibility': 'visible'}
 
 
+
 # Callbacks to clear search bar
 @app.callback(
     Output('search_box', 'value'),
@@ -863,4 +823,4 @@ def clear_search(button):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=False)
